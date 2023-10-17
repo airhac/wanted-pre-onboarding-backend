20e116d05ed7e2e1804ac7a99d29f5302ef326aa
airhac <oooh32141@gmail.com>
2023-10-17 13:16:32 +0900

    1. 데이터 수정시 회사_id와 관련된 데이터 제외한 나머지 채용관련 데이터 수정가능

--- recruit/serializers.py
+++ recruit/serializers.py
@@ -3,9 +3,12 @@ from company.models import Company
 from rest_framework import serializers
 
 class RecruitSerializer(serializers.ModelSerializer):
+    #회사_id 관련된 데이터는 수정이 불가 일기만 가능합니다.
+    company_id = serializers.PrimaryKeyRelatedField(read_only=True)
     company_name = serializers.SerializerMethodField()
     country = serializers.SerializerMethodField()
     region = serializers.SerializerMethodField()
+    
     #회사_id 값에 대한 회사 이름을 serializer를 통해 보내줍니다.
     def get_company_name(self, obj):
         return obj.company_id.company_name
--- recruit/views.py
+++ recruit/views.py
@@ -13,11 +13,20 @@ class RecruitViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         return Recruit.objects.all()
     
+    #이 함수를 적용함으로써 detail부분에서는 내용을 볼수 있습니다.
+    def get_serializer_class(self):
+        # 리스트 액션에서만 recruit_con 필드를 제외한 시리얼라이저를 사용
+        if self.action == 'list':
+            class ListRecruitSerializer(RecruitSerializer):
+                class Meta(RecruitSerializer.Meta):
+                    fields = [field for field in RecruitSerializer.Meta.fields if field != 'recruit_con']
+            return ListRecruitSerializer
+        return RecruitSerializer
+    
     def list(self, request, *args, **kwargs):
         queryset = self.get_queryset()
         
         serializer = self.get_serializer(queryset, many=True)
-        print(serializer)
         return Response(serializer.data)
         
         
